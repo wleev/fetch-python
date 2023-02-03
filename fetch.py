044a7821fad305c558465ac90b667f71c2f6b250
Wesley <wlee@isabot.net>
2023-02-03 16:50:47 +0100

    Extract and print meta information for every fetch

--- fetch.py
+++ fetch.py
@@ -1,6 +1,8 @@
 import argparse
 import requests
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 def fetch_url_content(url):
     res = requests.get(url)
@@ -11,8 +13,23 @@ def save_url_content(url, content):
     filename = url_comp.hostname + '.html'
 
     with open(filename, 'w') as web_file:
-        web_file.write(content)    
+        web_file.write(content)
 
+def extract_meta(url, content):
+    meta = {}
+    url_comp = urlparse(url)
+    meta['site'] = url_comp.hostname
+    meta['last_fetch'] = datetime.utcnow().isoformat()
+
+    soup = BeautifulSoup(content, 'html.parser')
+    content_links = soup.find_all('a') # find all links
+    content_img = soup.find_all('img')
+    
+    meta['num_links'] = len(content_links)
+    meta['images'] = len(content_img)
+    
+    return meta
+    
 parser = argparse.ArgumentParser(
     prog="Fetch-python",
     description="Gets and saves websites locally"
@@ -27,5 +44,7 @@ if __name__ == "__main__":
     for url in urls:
         content = fetch_url_content(url)
         save_url_content(url, content)
+        meta = extract_meta(url, content)
+        print(meta)
 
 
